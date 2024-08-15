0f687705537d4b43f5d7f7b482660b896a670605
“Kyle <kylemboomer@gmail.com>
2024-08-15 12:01:12 -0500

    Establish First Seed

--- knex/migrations/20240815153338_create_stacks_table.js
+++ knex/migrations/20240815153338_create_stacks_table.js
@@ -5,6 +5,17 @@
 exports.up = function (knex) {
     return knex.schema.createTable('albums', function (table) {
         table.string('id')
+        table.string('albumName')
+        table.string('artist')
+        table.string('releaseDate')
+        table.string('genre')
+        table.string('label')
+        table.specificType('bandMembers', 'text[]')
+        table.boolean('isBandTogether')
+        table.string('rollingStoneReview')
+        table.integer('albumsSold')
+        table.string('youTubeAlbumURL')
+        table.string('imgURL')
 
     })
 };
---
+++ knex/seeds/01_albums.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ *
+ */
+ const albums = require("../../albums")
+exports.seed = async function(knex) {
+ try {
+   await knex('albums').del()
+   await knex.batchInsert('albums', albums)
+  
+ } catch(error) {
+  console.log('Error:', error.message) 
+ }
+  
+  
+};
