8347ee445feb2c1fa7de44150786db142f58da05
“Kyle <kylemboomer@gmail.com>
2024-08-15 10:55:52 -0500

    Set Up Knex

---
+++ knex/migrations/20240815153338_create_stacks_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('albums', function (table) {
+        table.string('id')
+
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('albums') 
+};
